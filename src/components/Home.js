04d82ddd1180758014fb17fe65f3f03fabb6cdd8
Zachary Swensrude <swensrudez@gmail.com>
2024-03-03 15:05:06 -0700

    home content, project button prep

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Carousel } from "@material-tailwind/react";
+import { Button, Link } from "@nextui-org/react";
 import './components.css';
 import Lich from '../images/Projects/LichsLair.png';
 import Mac from '../images/Projects/Mac-UN.png';
@@ -8,36 +9,61 @@ import Space from '../images/Projects/SpaceEvaders.png'
 
 const Home = () => {
   return (
-    <div className="carousel">
-      <p>Home</p>
-      <Carousel 
-        className="rounded-xl"
-        autoplay={true}
-        autoplayDelay={5000}
-        loop={true}
-      >
-        <img
-          src={Lich}
-          alt="image 1"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src={Mac}
-          alt="image 2"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src={Ducky}
-          alt="image 3"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src={Space}
-          alt="image 3"
-          className="h-full w-full object-cover"
-        />
-      </Carousel>
-    </div>
+    <>
+      <div className="carousel">
+        <p className="headerText">
+          Welcome to my site! Here is a preview of some projects I have worked on during my time at MacEwan University!
+        </p>
+        <br />
+        <Carousel 
+          className="rounded-xl"
+          autoplay={true}
+          autoplayDelay={5000}
+          loop={true}
+        >
+          <>
+            <p className="carouselText">Lich's Lair: A 2D Roguelike made in Unity</p>
+            <img
+              src={Lich}
+              alt="Lich's Lair tutorial room"
+              className="h-full w-full object-cover"
+            />
+            {/* <Button className="imageButton" color="secondary">More Info</Button> */}
+          </>
+          <>
+            <p className="carouselText">Mac-UN: A Model UN Conference Webapp</p>
+            <img
+              src={Mac}
+              alt="Mac-UN web app front page"
+              className="h-full w-full object-cover"
+            />
+            {/* <Button className="imageButton" color="secondary">More Info</Button> */}
+          </>
+          <>
+            <p className="carouselText">DuckyBot: A 3D printed human-following robot</p>
+            <img
+              src={Ducky}
+              alt="Ducky bot, a 3d printed robot in the shape of a duck"
+              className="h-full w-full object-cover"
+            />
+            {/* <Button className="imageButton" color="secondary">More Info</Button> */}
+          </>
+          <>
+            <p className="carouselText" color="secondary">Space Evaders: A 3D endless runner game created in Unity</p>
+            <img
+              src={Space}
+              alt="in game screenshot from space evaders"
+              className="h-full w-full object-cover"
+            />
+            {/* <Button className="imageButton" color="secondary">More Info</Button> */}
+          </>
+        </Carousel>
+        <br />
+        <p className="descriptionText">
+          I expect to graduate in June 2024, and have been keeping extra busy during my last semester with various classes, projects, and at my internship as a full-stack developer at Dominarlo
+        </p>
+      </div>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NextUIProvider>
-      <main className="purple-dark text-foreground bg-background">
+      <main className="dark text-foreground bg-background">
         <App />
       </main>
     </NextUIProvider>
