f0cd5c8d99a1868552221380d6d44b82ae005c7f
Zachary Swensrude <swensrudez@gmail.com>
2024-03-02 21:07:41 -0700

    install NextUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h3>Welcome to Zach's portfolio! Check out some of my previous works below!</h3>
       </header>
     </div>
   );
