4ae601dfdb30057f0c1f1ac5d2b9550a206fdd29
Zachary Swensrude <swensrudez@gmail.com>
2024-03-03 17:47:21 -0700

    add buttons to project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,11 @@
 import React from "react";
-// import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import {Button} from "@nextui-org/react";
 import {
   Card,
   CardHeader,
   CardBody,
   CardFooter,
   Typography,
-  Avatar,
   Tooltip,
 } from "@material-tailwind/react";
 import projectList from '../projects.json';
@@ -41,16 +40,30 @@ const Projects = () => {
                 </Typography>
               </CardBody>
               <CardFooter className="flex items-center justify-between">
-                <div className="flex items-center -space-x-3">
-                  <Tooltip content="See the code!">
-                    <Avatar
-                      size="sm"
-                      variant="circular"
-                      alt="tania andrew"
-                      src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-                      className="border-2 border-white hover:z-10"
-                    />
-                  </Tooltip>
+                <div className="flex items-center">
+                  {project.github !== "" && (
+                    <Tooltip content="See the code!">
+                      <Button 
+                        isIconOnly 
+                        color="primary" 
+                        onClick={()=> window.open(project.github, "_blank")}
+                        variant="light"
+                      >
+                        <img id="icon" src={"/images/GitHub-logo.png"} alt="github logo"/>
+                      </Button>
+                    </Tooltip>
+                  )}
+                  {project.link !== "" && (
+                    <Tooltip content="See it in action!">
+                      <Button 
+                        className="projectButton bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+                        color="success" 
+                        onClick={()=> window.open(project.link, "_blank")}
+                      >
+                        {project.action}
+                      </Button>
+                    </Tooltip>
+                  )}
                 </div>
                 <Typography className="font-normal">{project.date}</Typography>
               </CardFooter>
