8f1dbf02321d6b20a3617fd20bb2ff71edf945ea
Zachary Swensrude <swensrudez@gmail.com>
2024-05-06 21:19:21 -0600

    finish about me section

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {Image} from "@nextui-org/react";
 import "./components.css";
 
 const About = () => {
+
   return (
     <>
       <p className="headerText">
@@ -18,7 +19,7 @@ const About = () => {
           <br/>
           <p>
             When it comes to programming, I am most experienced in Full Stack and Game Development (as you can 
-            probably tell from my Projects section), but I also enjoy all types of development, and even the 
+            probably tell from my Projects section), but I enjoy all types of development, as well as the 
             research side of the field. I made sure to take as many different programming classes as I could 
             during my time at MacEwan so that I could take on any challenge in (almost) any language and succeed!
           </p>
@@ -40,9 +41,33 @@ const About = () => {
       </div>
       <div className="about-bottom">
         <p>
-          Not only is my major nerdy, but I am too, with hobbies like Dungeons & Dragons, playing videogames, and 
-          reading manga. More importantly, I genuinely enjoy working hard, as I am someone who would rather be busy
-          than bored, and I take pride in my work.
+          I am someone who would rather be busy than bored, and I enjoy taking pride in my work. 
+          When not working, I can be found enjoying my hobbies like Dungeons & Dragons, playing 
+          videogames, reading manga, and hiking. 
+        </p>
+        <br/>
+        <p>
+          If you have the time, play some of my games! Lich's Lair is my most fleshed out game and a successful run
+          will likely take around 20 minutes (if you can beat it). Space Evaders is much simpler and better for shorter 
+          sessions, as it is an infinite runner. Make sure to submit your highscore if you give it a go! Both have 
+          input forms below the game that we used for some testing which you can also answer if you want to help us 
+          out! The links to both can be found on my Projects page.
+        </p>
+        <br/>
+        <div className="image-wrapper">
+          <img 
+            className="image-before" 
+            src={`${process.env.PUBLIC_URL}/images/Cats.jpg`} 
+            alt="logo" 
+          />
+          <img 
+            className="image-hover" 
+            src={`${process.env.PUBLIC_URL}/images/CatsClose.jpg`} 
+            alt="logo" 
+          />
+        </div>
+        <p className="fig" >
+          Fig. 1. My cats that got me through University
         </p>
       </div>
     </>
